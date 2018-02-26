a518e18bcbd4c60dcf132b910eac6261ebfa05d6
Acer <Acer@ACER-PC>
2018-02-26 10:30:50 +0800

    Add project files.

---
+++ T.Silk Scarves/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace T.Silk_Scarves.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ T.Silk Scarves/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="T.Silk_Scarves.Global" Language="C#" %>
